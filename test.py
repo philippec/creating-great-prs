981f92901b78e46a55487c180ede5f3a859352d2
Philippe Casgrain <philippe.casgrain@lightspeedhq.com>
2020-11-16 19:21:32 -0500

    INNO-303 Replaced shell test with python test
    Python tests include reporting, which can be seen in CircleCI.

---
+++ test.py
@@ -0,0 +1,21 @@
+import random
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+class SimpleTest(unittest.TestCase):
+    @unittest.skip("demonstrating skipping")
+    def test_skipped(self):
+        self.fail("shouldn't happen")
+
+    def test_pass(self):
+        self.assertEqual(10, 7 + 3)
+
+    @unittest.skip("get a green build")
+    def test_fail(self):
+        self.assertEqual(11, 7 + 3)
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
