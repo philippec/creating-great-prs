c1db84a191432f6bccadb4bd425e7c745c059f28
Philippe Casgrain <philippe.casgrain@lightspeedhq.com>
2020-11-18 23:11:05 -0500

    INNO-303 Updated python script
    Now properly tests characters, places, and tech

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ try:
 except ImportError:
     import unittest
 import emoji
+import re
+import os
 
 
 class BookTest(unittest.TestCase):
@@ -12,20 +14,40 @@ class BookTest(unittest.TestCase):
         with open('Book/Chapters/01.md', 'r') as file:
             self.chapter01 = file.read().replace('\n', '')
 
-    def test_chapter01(self):
-        self.assertEqual(self.chapter01, '# Chapter 1It was a dark and stormy night...')
+    def stem(self, word):
+        for suffix in ['s']:
+            if word.endswith(suffix):
+                return word[:-len(suffix)]
+            return word
 
-    @unittest.skip("not implemented yet")
     def test_characters(self):
-        self.fail("not implemented yet")
+        allPeopleRaw = re.findall(r'👤(\w+)', self.chapter01)
+        allPeople = set([person.lower() for person in allPeopleRaw])
+        knownPeopleRaw = os.listdir('Book/Characters')
+        knownPeople = [os.path.splitext(person)[0].lower() for person in knownPeopleRaw]
+        for onePerson in allPeople:
+            if not (onePerson in knownPeople):
+                self.fail('{} not found in {}'.format(onePerson, knownPeople))
 
-    @unittest.skip("not implemented yet")
     def test_places(self):
-        self.fail("not implemented yet")
+        allPlacesRaw = re.findall(r'📍(\w+)', self.chapter01)
+        allPlaces = set([place.lower() for place in allPlacesRaw])
+        knownPlacesRaw = os.listdir('Book/Places')
+        knownPlaces = [os.path.splitext(place)[0].lower() for place in knownPlacesRaw]
+        for onePlace in allPlaces:
+            if not (onePlace in knownPlaces):
+                if not (self.stem(onePlace) in knownPlaces):
+                    self.fail('{} not found in {}'.format(onePlace, knownPlaces))
 
-    @unittest.skip("not implemented yet")
     def test_tech(self):
-        self.fail("not implemented yet")
+        allTechRaw = re.findall(r'🔧(\w+)', self.chapter01)
+        allTech = set([tech.lower() for tech in allTechRaw])
+        knownTechRaw = os.listdir('Book/Tech')
+        knownTech = [os.path.splitext(tech)[0].lower() for tech in knownTechRaw]
+        for oneTech in allTech:
+            if not (oneTech in knownTech):
+                if not (self.stem(oneTech) in knownTech):
+                    self.fail('{} not found in {}'.format(oneTech, knownTech))
 
 if __name__ == '__main__':
     import xmlrunner
