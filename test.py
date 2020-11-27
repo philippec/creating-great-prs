875436bf66365dd2a14de9d0901c1bd7a2bdd06a
Philippe Casgrain <philippe.casgrain@lightspeedhq.com>
2020-11-26 23:54:21 -0500

    WIP tests for chapter 2

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ class BookTest(unittest.TestCase):
     def setUp(self):
         with open('Book/Chapters/01.md', 'r') as file:
             self.chapter01 = file.read().replace('\n', '')
+        with open('Book/Chapters/02.md', 'r') as file:
+            self.chapter02 = file.read().replace('\n', '')
 
     def stem(self, word):
         for suffix in ['s']:
@@ -49,6 +51,34 @@ class BookTest(unittest.TestCase):
                 if not (self.stem(oneTech) in knownTech):
                     self.fail('{} not found in {}'.format(oneTech, knownTech))
 
+    def test_characters_chapter_02(self):
+        allPeopleRaw = re.findall(r'👤(\w+)', self.chapter02)
+        allPeople = set([person.lower() for person in allPeopleRaw])
+        knownPeopleRaw = os.listdir('Book/Characters')
+        knownPeople = [os.path.splitext(person)[0].lower() for person in knownPeopleRaw]
+        for onePerson in allPeople:
+            if not (onePerson in knownPeople):
+                self.fail('{} not found in {}'.format(onePerson, knownPeople))
+
+    def test_places_chapter_02(self):
+        allPlacesRaw = re.findall(r'📍(\w+)', self.chapter02)
+        allPlaces = set([place.lower() for place in allPlacesRaw])
+        knownPlacesRaw = os.listdir('Book/Places')
+        knownPlaces = [os.path.splitext(place)[0].lower() for place in knownPlacesRaw]
+        for onePlace in allPlaces:
+            if not (onePlace in knownPlaces):
+                if not (self.stem(onePlace) in knownPlaces):
+                    self.fail('{} not found in {}'.format(onePlace, knownPlaces))
+
+    def test_tech_chapter_02(self):
+        allTechRaw = re.findall(r'🔧(\w+)', self.chapter02)
+        allTech = set([tech.lower() for tech in allTechRaw])
+        knownTechRaw = os.listdir('Book/Tech')
+        knownTech = [os.path.splitext(tech)[0].lower() for tech in knownTechRaw]
+        for oneTech in allTech:
+            if not (oneTech in knownTech):
+                if not (self.stem(oneTech) in knownTech):
+                    self.fail('{} not found in {}'.format(oneTech, knownTech))
 
 if __name__ == '__main__':
     import xmlrunner
